002cd42984f772d8c8c95a85536dee529729f915
cgp-client <code.gen.pro.system@gmail.com>
2024-08-24 19:31:34 +0300

    Adding models/car.js

---
+++ models/car.js
@@ -0,0 +1,9 @@
+class Car {
+  constructor(id, make, model, year, availability) {
+    this.id = id;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.availability = availability;
+  }
+}
